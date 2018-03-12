e04c29ba43c05acaa9f7c7732d680aec34319e3b
DJBZOMBIE <djbzombie@hotmail.com>
2018-03-12 20:05:54 -0300

    mod html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'UNIVAS';
+  
+  aluno = {
+    nome : 'Daniel',
+    cpf : '111.111.111-11',
+    telefone : '11111-1111',
+    email : 'Danieltest@gmail.com',
+    dataNascimento : '21/04/1993'
+  };
+
 }
